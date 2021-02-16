13d2bb1e0234dffa16c8dc57404b7dc31f118e46
wcjennyng <79124405+wcjennyng@users.noreply.github.com>
2021-02-15 21:30:36 -0500

    Add files via upload

---
+++ todos-LS.js
@@ -0,0 +1,70 @@
+const form = document.querySelector('#todo-list')
+const input = document.querySelector('#list')
+const taskList = document.querySelector('#task-list')
+const button = document.querySelector('#button')
+document.addEventListener('DOMContentLoaded', getTodos)
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const newTask = document.createElement('li')
+    const removeBtn = document.createElement('button')
+    removeBtn.innerText = 'X'
+    newTask.innerText = input.value 
+    newTask.append(removeBtn)
+    taskList.append(newTask)
+    saveLocalTodos(input.value)
+    input.value = ""
+})
+ 
+
+
+taskList.addEventListener('click', function(e) {
+        
+    if (e.target.tagName === 'LI') {
+            e.target.classList.toggle('completed')
+    } else if (e.target.tagName==='BUTTON') {
+            removeLocalTodos(e.target.parentElement)
+            e.target.parentElement.remove()
+    }
+})
+ 
+function saveLocalTodos(todo) {
+        let todos
+        if(localStorage.getItem('todos')===null){
+                todos = []
+        } else{
+                todos = JSON.parse(localStorage.getItem('todos'))
+        }
+        todos.push(todo)
+        localStorage.setItem('todos',JSON.stringify(todos))
+}
+ 
+function getTodos() {
+    let todos;
+    if(localStorage.getItem('todos')===null){
+        todos = []
+} else{
+        todos = JSON.parse(localStorage.getItem('todos'))
+}
+todos.forEach(function(todo){
+    const newTask = document.createElement('li')
+    const removeBtn = document.createElement('button')
+    removeBtn.innerText = 'X'
+    newTask.innerText = todo
+    newTask.append(removeBtn)
+    taskList.append(newTask)      
+})
+}
+
+function removeLocalTodos(todo) {
+        let todos;
+        if(localStorage.getItem('todos') === null) {
+            todos = []
+        }
+        else {
+            todos = JSON.parse(localStorage.getItem('todos'))
+        }
+        todos.splice(todo.childNodes[0],1)
+        localStorage.setItem('todos',JSON.stringify(todos))
+    }
